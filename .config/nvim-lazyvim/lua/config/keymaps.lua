59480708d24a72bf0a304a0bd0994497a6e09795
Lambdaphile <lambdaphile@pm.me>
2025-08-20 20:28:38 +0300

    feat(lazyvim): configure colorscheme, dashboard, lualine, and etc.

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map(
+  { "i", "x", "n", "s" },
+  "<leader>fs",
+  "<cmd>wa<cr><esc>",
+  { desc = "Save All Files" }
+)
+map(
+  { "i", "x", "n", "s" },
+  "<C-s>",
+  "<cmd>wa<cr><esc>",
+  { desc = "Save All Files" }
+)
---
+++ .config/nvim-lazyvim/lua/plugins/overrides.lua
@@ -0,0 +1,104 @@
+local u = require("utils")
+
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "catppuccin" },
+  },
+
+  {
+    "catppuccin/nvim",
+    opts = { flavour = "mocha" },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = {
+          statusline = {
+            "dashboard",
+            "alpha",
+            "ministarter",
+            "snacks_dashboard",
+          },
+        },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = {
+          { "mode", separator = { left = "" }, right_padding = 2 },
+        },
+        lualine_b = { "branch" },
+        lualine_c = {
+          { LazyVim.lualine.pretty_path() },
+          "diagnostics",
+        },
+        lualine_x = {
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+    },
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+
+  {
+    "echasnovski/mini.starter",
+    opts = {
+      header = function()
+        local fortune = vim.fn.system("fortune -s -n 40 | cowsay")
+        local pad = string.rep(" ", 8)
+        local fortune_lines = {}
+
+        for line in fortune:gmatch("[^\r\n]+") do
+          table.insert(fortune_lines, pad .. line)
+        end
+
+        return table.concat(fortune_lines, "\n")
+      end,
+      items = {
+        u.new_section("ff → Find file", LazyVim.pick(), "Telescope"),
+        u.new_section(
+          "rf → Recent files",
+          LazyVim.pick("oldfiles"),
+          "Telescope"
+        ),
+        u.new_section(
+          "ft → Find text",
+          LazyVim.pick("live_grep"),
+          "Telescope"
+        ),
+        u.new_section("n  → New file", "ene | startinsert", "Built-in"),
+        u.new_section("q  → Quit", "qa", "Built-in"),
+        u.new_section("c  → Config", LazyVim.pick.config_files(), "Config"),
+        u.new_section("l  → Lazy", "Lazy", "Config"),
+        u.new_section("le → Lazy extras", "LazyExtras", "Config"),
+        u.new_section(
+          "rs → Restore session",
+          [[lua require("persistence").load()]],
+          "Session"
+        ),
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+}
