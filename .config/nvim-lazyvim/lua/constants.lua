e9b12cc67a032f2cc2c9a719a72766e1a8e36af8
Lambdaphile <lambdaphile@pm.me>
2025-08-29 18:58:46 +0300

    chore(lazyvim): exclude .DS_Store in picker config

--- .config/nvim-lazyvim/lua/constants.lua
+++ .config/nvim-lazyvim/lua/constants.lua
@@ -3,7 +3,11 @@ local M = {}
 M.PICKER_SETTINGS = {
   hidden = true,
   ignored = true,
-  exclude = { ".git" },
+  exclude = { ".git", ".DS_Store" },
 }
 
+M.LUALINE_SEPARATORS = {
+  left = "",
+  right = "",
+}
 return M
